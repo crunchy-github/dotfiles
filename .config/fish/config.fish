43fbbffc151defb25505efae90a09bcc0cede188
Andreas Büsching <a.buesching@best-blu.de>
2024-02-22 15:01:01 +0100

    add ~/.local/bin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# add .local/bin to PATH
+fish_add_path --path ~/.local/bin
+
+# init starshop
 starship init fish | source
